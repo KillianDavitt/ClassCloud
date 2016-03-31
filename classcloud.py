f5a6397b443331d4ee277b661829a9fd1d809eb2
barischj <barischj@tcd.ie>
2016-03-31 14:08:33 +0100

    restructure into single file

---
+++ classcloud.py
@@ -0,0 +1,43 @@
+import flask
+import flask_sqlalchemy
+import os
+
+DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+HOST = "127.0.0.1"
+PORT = 3000
+DEBUG = True
+
+app = flask.Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/classcloud.sqlite"
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+##########
+# Models #
+##########
+
+class File(db.Model):
+  id = db.Column(db.String(), primary_key=True)
+  path = db.Column(db.String(), unique=True)
+
+  def __init__(self, id, path):
+    self.id = id = username
+    self.path = path
+
+##########
+# Routes #
+##########
+
+# Route to test connections to this server.
+@app.route("/test")
+def test():
+  return "Success.", 200
+
+#######
+# Run #
+#######
+
+if __name__ == "__main__":
+	db.drop_all()
+	db.create_all()
+	app.run(host=HOST, port=PORT)
