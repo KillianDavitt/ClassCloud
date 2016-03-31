619a3f48712c6964c9e72db2261ef23142597c66
Killian Davitt <davittk@tcd.ie>
2016-03-31 14:55:16 +0100

    More tests

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import classcloud
 import unittest
-
+from io import StringIO
 token = None
 
 class ServerTestCase(unittest.TestCase):
@@ -11,13 +11,18 @@ class ServerTestCase(unittest.TestCase):
     classcloud.db.create_all()
     self.client = classcloud.app.test_client()
 
-  def test_list_files(self):
+  """def test_list_files(self):
     # valid data
   	result = self.client.post("/signup", data=user_json, content_type="application/json")
   	self.assertEqual(result.status_code, 200)
     # already signed up
   	result = self.client.post("/signup", data=user_json, content_type="application/json")
   	self.assertEqual(result.status_code, 400)
+"""
+  def test_put_file(self):
+    post_data = dict(token="hjdh34", file=(StringIO('my file contents'), 'helloworld.txt'), path="/john")
+    result = self.client.post('/put_file', data=post_data, content_type="application/json")
+    print(result)
 
 if __name__ == "__main__":
   token = classcloud.get_token()
