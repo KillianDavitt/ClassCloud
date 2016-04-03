4301ca96ce3044b9df7b51b386f41422fce2a3f5
barischj <barischj@tcd.ie>
2016-04-03 18:04:35 +0100

    client python library for interacting with server

---
+++ client_lib.py
@@ -0,0 +1,29 @@
+import requests
+import urllib.parse
+
+
+# Return a file with given ID from the file server.
+def get_file(url, token, id_):
+  url = urllib.parse.urljoin(url, "/get_file")
+  data = {"token": token, "id": id_}
+  response = requests.get(url, json=data)
+  print(response.text)
+  return response.content
+
+
+# Return a list of (file IDs, file paths) from the file server.
+def list_files(url, token):
+  url = urllib.parse.urljoin(url, "/list_files")
+  response = requests.get(url, json={"token": token})
+  print(response.text)
+  return response.json()["files"]
+
+
+# Upload a file to the file server, return True on success else False.
+def put_file(url, token, path, filename, file_bytes):
+  url = urllib.parse.urljoin(url, "/put_file")
+  files = {"file": (filename, file_bytes)}
+  data = {"token": token, "path": path}
+  response = requests.post(url, files=files, data=data)
+  print(response.text)
+  return response.status_code == 200
