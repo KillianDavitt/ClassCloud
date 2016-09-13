83d53f378dacf58878302e1e3ea57f27ec81bb70
Killian Davitt <davittk@tcd.ie>
2016-09-13 15:10:31 +0100

    Fixed small error in list, added models in separate file

--- models.py
+++ models.py
@@ -1,2 +1,10 @@
-
-
+class File(db.Model):
+  id = db.Column(db.String(), primary_key=True)
+  path = db.Column(db.String())
+  filename = db.Column(db.String(20))
+ 
+  def __init__(self, id, path, filename):
+    self.id = id
+    self.path = path
+    self.filename = filename
+ 
